4af0d751e6bb9f934f1288f6e692efa4b5d95131
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-02 16:56:30 +0200

    add title in helloworld component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <HeroJumbotron />
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeroJumbotron from './components/HeroJumbotron.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeroJumbotron
+
+
   }
 }
 </script>
---
+++ src/components/HeroJumbotron.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="https://picsum.photos/200/300" alt="foto_random">
+</template>
